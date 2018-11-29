458ced56940f2d10de869c5fe74aebe189532b36
parul.dhawan <parul.dhawan@snapdeal.com>
2018-11-29 15:40:42 +0530

    updaet

--- vars/parul-shared-library.groovy
+++ vars/parul-shared-library.groovy
@@ -1,29 +1,7 @@
 #!/usr/bin/groovy
-import groovy.json.JsonSlurper
-
-@NonCPS
-def parseJson(text) {
-  return new JsonSlurper().parseText(text)
-}
 
 def call(body) {
   echo "Running in declarative pipeline"
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
-
-  def log_level = "quiet"
-  if(DEBUG) {
-    log_level = "debug"
-
-
-  stage('setup') {
-    node("env:dev",{
-          // create a function to do this. @todo princetyagi
-          sh(echo "HII")
-      }
-    })
   }
 
 
